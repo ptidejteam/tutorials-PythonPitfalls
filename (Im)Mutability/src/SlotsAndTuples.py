13a99597b848ca83325e5fb788164a8723ba77ae
yann-gael <yann-gael.gueheneuc@concordia.ca>
2024-05-31 14:33:26 -0400

    Truly immitable classes (and their instances)

--- (Im)Mutability/src/SlotsAndTuples.py
+++ (Im)Mutability/src/SlotsAndTuples.py
@@ -1,4 +1,5 @@
 import unittest
+import inspect
 
 
 class Immutable1():
@@ -47,6 +48,59 @@ class ImmutableWithMC3(metaclass=ImmutableClassMetaClass3):
     pass
 
 
+class ImmutableClassMetaClass4(type):
+
+    def __setattr__(self, name, value):
+        raise AttributeError("Sorry, you cannot add class variables to this class") 
+
+    def __setattr__for_class(self, name, value):
+        raise AttributeError("Sorry, you cannot add instance variables to this class")
+
+    def __new__(cls, name, bases, attrs):
+        cls = type.__new__(cls, name, bases, attrs)
+        super().__setattr__('__setattr__', ImmutableClassMetaClass4.__setattr__for_class)
+        return cls
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(self)
+
+
+class ImmutableWithMC4(metaclass=ImmutableClassMetaClass4):
+    cls_var = 42
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__()
+        self.__dict__['ins_var'] = 24
+
+
+class ImmutableClassMetaClass5(type):
+
+    def __setattr__(self, name, value):
+        raise AttributeError("Sorry, you cannot add class variables to this class") 
+
+    def __setattr__for_class(self, name, value):
+        if inspect.stack()[1].function != "__init__":
+            raise AttributeError("Sorry, you cannot add instance variables to this class")
+        else:
+            object.__setattr__(self, name, value)
+
+    def __new__(cls, name, bases, attrs):
+        cls = type.__new__(cls, name, bases, attrs)
+        super().__setattr__('__setattr__', ImmutableClassMetaClass5.__setattr__for_class)
+        return cls
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(self)
+
+
+class ImmutableWithMC5(metaclass=ImmutableClassMetaClass5):
+    cls_var = 42
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__()
+        self.ins_var = 24
+
+
 class Test(unittest.TestCase):
 
     def testImmutable1(self):
@@ -69,8 +123,8 @@ class Test(unittest.TestCase):
 
     def testImmutableWithMC1(self):
         self.assertTrue('__dict__' in dir(ImmutableWithMC1))
-        ImmutableWithMC1.cls_var = 42
-        self.assertEqual(ImmutableWithMC1.cls_var, 42)
+        ImmutableWithMC1.cls_var1 = 42
+        self.assertEqual(ImmutableWithMC1.cls_var1, 42)
         self.assertTrue('__dict__' in dir(ImmutableWithMC1))
 
         a = ImmutableWithMC1()
@@ -79,8 +133,8 @@ class Test(unittest.TestCase):
 
     def testImmutableWithMC2(self):
         self.assertFalse('__dict__' in dir(ImmutableWithMC2))
-        ImmutableWithMC2.cls_var = 42
-        self.assertEqual(ImmutableWithMC2.cls_var, 42)
+        ImmutableWithMC2.cls_var2 = 42
+        self.assertEqual(ImmutableWithMC2.cls_var2, 42)
         self.assertFalse('__dict__' in dir(ImmutableWithMC2))
 
         a = ImmutableWithMC2()
@@ -92,13 +146,45 @@ class Test(unittest.TestCase):
 
     def testImmutableWithMC3(self):
         self.assertFalse('__dict__' in dir(ImmutableWithMC3))
-        ImmutableWithMC3.cls_var = 42
-        self.assertEqual(ImmutableWithMC3.cls_var, 42)
+        ImmutableWithMC3.cls_var3 = 42
+        self.assertEqual(ImmutableWithMC3.cls_var3, 42)
         self.assertFalse('__dict__' in dir(ImmutableWithMC3))
 
         a = ImmutableWithMC3()
         try:
             a.inst_var3 = 42
+            self.assertTrue(False)
+        except AttributeError:
+            self.assertTrue(True)
+
+    def testImmutableWithMC4(self):
+        self.assertEqual(ImmutableWithMC4.cls_var, 42)
+        try:
+            ImmutableWithMC4.cls_var4 = 42
+            self.assertTrue(False)
+        except AttributeError:
+            self.assertTrue(True)
+
+        a = ImmutableWithMC4()
+        self.assertEqual(a.ins_var, 24)
+        try:
+            a.inst_var4 = 42
+            self.assertTrue(False)
+        except AttributeError:
+            self.assertTrue(True)
+
+    def testImmutableWithMC5(self):
+        self.assertEqual(ImmutableWithMC5.cls_var, 42)
+        try:
+            ImmutableWithMC4.cls_var5 = 42
+            self.assertTrue(False)
+        except AttributeError:
+            self.assertTrue(True)
+
+        a = ImmutableWithMC5()
+        self.assertEqual(a.ins_var, 24)
+        try:
+            a.inst_var5 = 42
             self.assertTrue(False)
         except AttributeError:
             self.assertTrue(True)
